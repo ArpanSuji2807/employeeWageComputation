2d7ee125f9225a1d77660fc5e6af44af4d60728c
Praan26 <arpanap102@gmail.com>
2022-04-07 10:18:22 +0530

    [Arpan] Add: Employee Attendance added

--- employeeWageComputation/employeeWageComputation/Program.cs
+++ employeeWageComputation/employeeWageComputation/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace employeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            wageComputation computation = new wageComputation();
+            computation.EmployeeAttendance();
+        }
+    }
+}
---
+++ employeeWageComputation/employeeWageComputation/wageComputation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeeWageComputation
+{
+    public class wageComputation
+    {
+        const int IS_PRESENT = 0;
+        public void EmployeeAttendance()
+        {
+            
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+        }
+    }
+}
