1c84c1c810cbd7f2c7a77b5caed43ec8362539bb
Praan26 <arpanap102@gmail.com>
2022-04-19 14:11:21 +0530

    [Arpan] Add: Employee wage for multiple companies added

---
+++ employeeWageComputation/employeeWageComputation/EmpWageMultipleCompanies.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeeWageComputation
+{
+    public class EmpWageMultipleCompanies
+    {
+        const int IS_PART_TIME = 1;
+        const int IS_FULL_TIME = 2;
+        public void CompaniesWage(string company, int wage_per_hour, int working_days_per_month, int maximum_working_hours)
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs < maximum_working_hours && totalWorkingDays < working_days_per_month)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs += 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs += 4;
+                        break;
+                    default:
+                        break;
+
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#: " + totalWorkingDays + " EmpHrs: " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * wage_per_hour;
+            Console.WriteLine("Total empWage for a month is: " + totalEmpWage);
+        }
+    }
+}
--- employeeWageComputation/employeeWageComputation/Program.cs
+++ employeeWageComputation/employeeWageComputation/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 
-namespace employeeWageComputation
+namespace employeeWageComputation;
+
+class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            wageComputation monthlyWage = new wageComputation();
-            monthlyWage.EmployeeAttendance();
-            monthlyWage.MonthlyEmpWage();
-        }
+    static void Main(string[] args)
+    { 
+       EmpWageMultipleCompanies companies = new EmpWageMultipleCompanies();
+        Console.WriteLine("Company name: ");
+        string company = Console.ReadLine();
+        Console.WriteLine("Rate per hour: ");
+        int wage_per_hour = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Monthly Working Days: ");
+        int working_days_per_month = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Maximum working hours: ");
+        int maximum_working_hours = Convert.ToInt32(Console.ReadLine());
+        companies.CompaniesWage(company, wage_per_hour, working_days_per_month, maximum_working_hours);
     }
 }
